a4ed69ad8f15eb59d02f7a112f1c9703f19e1e13
hamza <hamza@DESKTOP-FP9DQFP>
2019-12-24 14:12:18 +0500

    Add project files.

---
+++ UokSemesterSystem/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace UokSemesterSystem
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(strcon);
+            conn.Open();
+            TextBox1.Text = "Connection has een opened";
+            conn.Close();
+
+        }
+    }
+}
